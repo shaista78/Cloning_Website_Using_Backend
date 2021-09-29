e90a025c860f6160936c404cec7aaa2bd8c98f97
rht16 <rohitkrrt@gmail.com>
2021-09-29 20:29:26 +0530

    login

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const connect = require('./configs/db');
+const register = require('./src/models/signup');
+
+
 
 const Products = require('./models/admin');
 
@@ -32,31 +35,95 @@ app.get('/', (req,res)=>{
     res.render('home');
 });
 
+ 
+app.use("sign", register);
 
-app.get('/signup.hbs', (req,res)=>{
+app.get('/signup', (req,res)=>{
     res.render('signup')
     
 });
 
-app.get('/address.hbs', (req,res)=>{
-    res.render('address')
+
+// 
+
+app.get('/signup.hbs', (req, res)=>{
+    res.render('signup')
+})
+// app.get('/address.hbs', (req,res)=>{
+//     res.render('address')
+// })
+
+// app.get('/AfterLogin.hbs', (req,res)=>{
+//     res.render('AfterLogin')
+// })
+
+// app.get('/body_mist_women.hbs',(req,res)=>{
+//     res.render('body_mist_women')
+// })
+
+// app.get('/card.hbs',(req,res)=>{
+//     res.render('card');
+// })
+
+// app.post('/register', register)
+
+// app.get('/admin', (req,res)=>{
+//     res.render('admin')
+// })
+
+
+app.post("/register", async (req, res) => {
+    try {
+       
+        const mobile = req.body.mobile;
+        const password = req.body.password;
+
+
+
+        if (mobile.length === 10 && password.length >= 8) {
+            const done = new register({
+                mobile: req.body.mobile,
+                password: req.body.password
+            });
+
+            const registerd = await done.save();
+            res.status(200).render("login")
+        } else {
+            res.send("please enter valid things")
+        }
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
 })
 
-app.get('/AfterLogin.hbs', (req,res)=>{
-    res.render('AfterLogin')
+app.get("/login", (req, res) => {
+    res.render('login')
+});
+
+//login partials
+
+app.post('/login', async (req, res) => {
+    try {
+        const det = req.body.ida;
+        const cred = req.body.cred;
+        
+        const userdetails = await register.findOne({ mobile: det });
+        if (userdetails.password === cred) {
+            res.render('AfterLogin')
+        } else {
+            res.send("wrong")
+        }
+        // res.send("user not found!")
+
+
+    } catch (err) {
+        res.status(400).send("user not found!");
+    }
 })
 
-app.get('/body_mist_women.hbs',(req,res)=>{
-    res.render('body_mist_women')
-})
 
-app.get('/card.hbs',(req,res)=>{
-    res.render('card');
-})
 
-app.get('/admin', (req,res)=>{
-    res.render('admin')
-})
 
 
 app.listen(3000, async() =>{
